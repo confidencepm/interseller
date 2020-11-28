17db2131645737ec09cab6072d31d0de79cbbd97
ConfidenceP <mukwindidzacp@gmail.com>
2020-11-29 01:13:48 +0200

    Worked on reading the file data(Task: #6) and created planet entity (Task: #2)

---
+++ src/main/java/za/co/dinoko/assignment/confidencemukwindidza/supportData/SupportData.java
@@ -0,0 +1,48 @@
+package za.co.dinoko.assignment.confidencemukwindidza.supportData;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import za.co.dinoko.assignment.confidencemukwindidza.model.Planet;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class SupportData {
+
+    public static void readingFile() throws IOException {
+
+        // Setting the path to excel file with planets data
+        File file = new File("src/main/resources/static/SupportData-V1.xlsx");
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fileInputStream);
+        XSSFSheet xssfSheet= xssfWorkbook.getSheetAt(0);
+
+        Iterator<Row> rowIterator = xssfSheet.iterator();
+        rowIterator.next();
+
+        while (rowIterator.hasNext()) {
+
+            Planet planet = new Planet();
+
+            Row row = rowIterator.next();
+
+            Iterator<Cell> cellIterator = row.cellIterator();
+
+            // Setting the planetNode from file
+            Cell cell = cellIterator.next();
+            planet.setPlanetNode(cell.toString());
+
+            // Setting the planetName from file
+            cell = cellIterator.next();
+            planet.setPlanetName(cell.toString());
+
+        }
+        xssfWorkbook.close();
+        fileInputStream.close();
+    }
+}
