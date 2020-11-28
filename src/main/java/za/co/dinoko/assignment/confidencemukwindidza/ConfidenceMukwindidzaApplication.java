1fcc97dabf5e089ff45b06165aa13b8c2d9aa123
Thabo Matjuda <thabo@anylytical.co.za>
2020-11-28 23:49:14 +0200

    1. Initial PUSH Yea!

---
+++ src/main/java/za/co/dinoko/assignment/confidencemukwindidza/ConfidenceMukwindidzaApplication.java
@@ -0,0 +1,13 @@
+package za.co.dinoko.assignment.confidencemukwindidza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfidenceMukwindidzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfidenceMukwindidzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/dinoko/assignment/confidencemukwindidza/ConfidenceMukwindidzaApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.dinoko.assignment.confidencemukwindidza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfidenceMukwindidzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
