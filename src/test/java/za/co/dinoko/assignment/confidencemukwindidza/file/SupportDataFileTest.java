d209e2cb414346dbd8e4015b17318481771ec1b9
Thabo Matjuda <thabo@anylytical.co.za>
2020-11-29 03:00:17 +0200

    #6 - Added some tests to try and match up the properties, PLANETS

--- src/test/java/za/co/dinoko/assignment/confidencemukwindidza/file/SupportDataFileTest.java
+++ src/test/java/za/co/dinoko/assignment/confidencemukwindidza/file/SupportDataFileTest.java
@@ -7,8 +7,7 @@ import za.co.dinoko.assignment.confidencemukwindidza.model.Planet;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class SupportDataFileTest {
@@ -17,11 +16,25 @@ public class SupportDataFileTest {
     @Autowired
     private SupportDataFile supportDataFile;
 
+    private List<Planet> planetList;
+
 
     @Test
-    public void testFileReading() {
-        List<Planet> planetList = supportDataFile.getPlanetList();
+    public void testThatWeCanReadFileIntoList() throws Exception {
+        planetList = supportDataFile.getPlanetList();
         assertNotNull( planetList);
         assertTrue( planetList.size() > 0);
+    }
+
+    @Test
+    public void testThat_J_IsFor_Mercury() throws Exception {
+        planetList = supportDataFile.getPlanetList();
+        Planet mercury = findPlanetByNode("J");
+        assertNotNull( mercury);
+        assertEquals( "Mercury", mercury.getPlanetName());
+    }
+
+    private Planet findPlanetByNode(String planetNodeArg) {
+        return planetList.stream().filter( planet -> planet.getPlanetNode().equals(planetNodeArg)).findFirst().get();
     }
 }
