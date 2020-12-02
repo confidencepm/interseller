efdd2026674cd6dbf966f88751b602a257e3103f
ConfidenceP <mukwindidzacp@gmail.com>
2020-12-02 13:57:29 +0200

    #5 Created the API resource to get the shortest path from A (Origin).

--- src/main/java/za/co/dinoko/assignment/confidencemukwindidza/resource/ShortestPathResource.java
+++ src/main/java/za/co/dinoko/assignment/confidencemukwindidza/resource/ShortestPathResource.java
@@ -1,4 +1,18 @@
 package za.co.dinoko.assignment.confidencemukwindidza.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.co.dinoko.assignment.confidencemukwindidza.service.ShortestPath;
+
+@RestController
 public class ShortestPathResource {
+
+    @Autowired
+    private ShortestPath shortestPath;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{destination}")
+    @ResponseBody
+    public String shortestPathToDestination(@PathVariable("destination") String destination) {
+        return shortestPath.shortestPathSearch(destination);
+    }
 }
